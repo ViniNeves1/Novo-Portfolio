b2be1cab4c38dd64112f18044e1628935b03b75d
ViniNeves1 <146591100+ViniNeves1@users.noreply.github.com>
2025-02-14 18:09:29 -0300

    Adicionado maior responsividade nos projetos
    Depois de muito trabalho consegui adicionar uma função para alterar a posição e o tamanho  das imagens conforme a tela é redimensionada

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 document.addEventListener('DOMContentLoaded', function() {
   const perfilPhoto = document.getElementById('perfil_foto');
   const descricao = document.getElementById('description');
-  const bioDiv = document.getElementsByClassName('my_bio')[0];
   const sobreImg = document.getElementsByClassName('sobre-img')[0];
   
     // Define os estilos iniciais ao carregar a página
@@ -11,7 +10,6 @@ document.addEventListener('DOMContentLoaded', function() {
     perfilPhoto.classList.add('float-l');
 
     if (window.innerWidth < 1024) {
-        bioDiv.classList.add('hidden');
         sobreImg.classList.add('hidden');
     }
 
@@ -21,7 +19,7 @@ document.addEventListener('DOMContentLoaded', function() {
   });
 
   document.addEventListener("DOMContentLoaded", function () {
-    const perfilFoto = document.getElementById("perfil_foto");
+    
     const descriptionDiv = document.getElementById("description");
     const textElement = document.getElementById("typed-text");
     const text = "Olá, eu sou o Vinícius e sou um desenvolvedor front-end em aprendizagem; Este é meu portfólio onde irei me apresentar a você.";
@@ -45,3 +43,39 @@ document.addEventListener('DOMContentLoaded', function() {
     descriptionDiv.classList.remove("hidden");
     typeWriter(); // Começa a digitação do texto automaticamente
 });
+
+const projects = document.getElementById('projects');
+
+
+function trocaClasse() {
+    if (window.innerWidth < 1024) {
+        projects.classList.remove('row');
+        projects.classList.add('col');
+    } else {
+        projects.classList.remove('col');
+        projects.classList.add('row');
+
+    }
+}
+
+trocaClasse();
+
+window.addEventListener('resize', trocaClasse);
+
+const imgProjetos = document.getElementsByClassName('img-projeto');
+
+function trocaTamanho() {
+    for (let imgProjeto of imgProjetos) {
+        if (window.innerWidth < 1024) {
+            imgProjeto.classList.remove('w-100');
+            imgProjeto.classList.add('w-50');
+        } else {
+            imgProjeto.classList.remove('w-50');
+            imgProjeto.classList.add('w-100');
+        }
+    }
+}
+
+trocaTamanho();
+
+window.addEventListener('resize', trocaTamanho);
